110a0e1b12f20ed7432e4de675b3daa59b3fb789
Todor Radev <todor.radev-ext@lexisnexis.com>
2022-12-05 14:08:00 +0200

    some refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Vamos Naganqnis
+            Vamos Naganqnisdd312
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
