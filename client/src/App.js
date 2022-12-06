86af7a30c8744b44d21c7b8b805e68eee742530d
Todor Radev <todor.radev-ext@lexisnexis.com>
2022-12-06 16:00:50 +0200

    change of title

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Vamos Naganqnisdd312
+            Version Kubernetes
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
